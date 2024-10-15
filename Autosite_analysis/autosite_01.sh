6159dc707c35ceeb4e4fcb5a9bb55751f4a9f95a
Evanspap <evanspap@yahoo.com>
2024-10-14 20:53:44 -0400

    update

---
+++ Autosite_analysis/autosite_01.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+for i in ../../Autosite_2/*/*_fp_*.pdb
+do
+	filename=$(basename -- "$i")
+	dir=$(dirname "$i")
+	name="${filename%.*}"
+	protein="${name%%_*}"
+	pocketnt="${name: -3}"
+	echo $i
+	echo $dir
+	echo $filename
+	echo $name
+	echo $protein
+	echo $pocketnt
+	#python -W ignore ../../pdb-seq.py $i > ./SEQ/${name%.*}.seq
+	
+done
