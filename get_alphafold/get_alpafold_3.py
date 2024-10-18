1d024984556a6ee9f6a71e1b73f9c2e73fd2d622
Evanspap <evanspap@yahoo.com>
2024-10-18 11:00:47 -0400

    new

--- get_alphafold/get_alpafold_3.py
+++ get_alphafold/get_alpafold_3.py
@@ -1,13 +1,20 @@
 import sys
 
+
+print("#!/bin/sh")
+
 f = open(sys.argv[1], "r")
 
 for ln in f:
   #print(x)
 	lnargs=ln.split()
 	out="curl -o "+lnargs[1]+".pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v1.pdb"
+	print(out)
 	out="curl -o "+lnargs[1]+".pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v2.pdb"
+	print(out)
 	out="curl -o "+lnargs[1]+".pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v3.pdb"
+	print(out)
 	out="curl -o "+lnargs[1]+".pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v4.pdb"
+	print(out)
 	out="curl -o "+lnargs[1]+".pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v5.pdb"
 	print(out)
