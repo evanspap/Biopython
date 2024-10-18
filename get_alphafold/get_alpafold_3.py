182a123151959775687f34746ed095a2f34002f5
Evangelos Papadopoulos <71100437+evanspap@users.noreply.github.com>
2024-10-18 12:56:32 -0400

    Update get_alpafold_3.py

--- get_alphafold/get_alpafold_3.py
+++ get_alphafold/get_alpafold_3.py
@@ -8,13 +8,13 @@ f = open(sys.argv[1], "r")
 for ln in f:
   #print(x)
 	lnargs=ln.split()
-	out="curl -o "+lnargs[1]+".pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v1.pdb"
+	out="curl -o "+lnargs[1]+"_v1.pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v1.pdb"
 	print(out)
-	out="curl -o "+lnargs[1]+".pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v2.pdb"
+	out="curl -o "+lnargs[1]+"_v2.pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v2.pdb"
 	print(out)
-	out="curl -o "+lnargs[1]+".pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v3.pdb"
+	out="curl -o "+lnargs[1]+"_v3.pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v3.pdb"
 	print(out)
-	out="curl -o "+lnargs[1]+".pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v4.pdb"
+	out="curl -o "+lnargs[1]+"_v4.pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v4.pdb"
 	print(out)
-	out="curl -o "+lnargs[1]+".pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v5.pdb"
+	out="curl -o "+lnargs[1]+"._v5pdb https://alphafold.ebi.ac.uk/files/AF-"+lnargs[0]+"-F1-model_v5.pdb"
 	print(out)
