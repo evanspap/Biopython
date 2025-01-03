0cfbd43f06d5afc83c3a07763de70914cc0a3f62
Evangelos Papadopoulos <71100437+evanspap@users.noreply.github.com>
2025-01-03 05:14:24 -0500

    Update make_genebank.sh

--- pdb_to_genbank/make_genebank.sh
+++ pdb_to_genbank/make_genebank.sh
@@ -5,7 +5,7 @@ for pdb_file in *.pdb; do
     # Check if the file exists and is a regular file
     if [ -f "$pdb_file" ]; then
         # Run the Python script on the current pdb file
-        python pdb_to_genebank.py "$pdb_file"
+        python pdb_to_genebank_AF.py "$pdb_file"
         echo "Processed: $pdb_file"
     fi
 done
